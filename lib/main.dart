6e1bbb9c5f32dacc33be79efed0213d675133c5e
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-11 12:43:09 +0900

    Apply main page base design

--- lib/main.dart
+++ lib/main.dart
@@ -11,32 +11,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Diary Flutter Demo',
+      title: '',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Diary Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text(''),
       ),
-      body: Container(),
+      body: Container(child: getPage()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: '',
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.today), label: '오늘'),
+            BottomNavigationBarItem(icon: Icon(Icons.calendar_today_rounded), label: '기록'),
+            BottomNavigationBarItem(icon: Icon(Icons.insert_chart), label: '통계'),
+          ],
+          onTap: (idx) {
+            setState(() {
+              selectIndex = idx;
+            });
+          }),
     );
+  }
+
+  Widget getPage() {
+    if (selectIndex == 0) {
+      return getTodayPage();
+    } else if (selectIndex == 1) {
+      return getHistoryPage();
+    } else {
+      return getChartPage();
+    }
+  }
+
+  Widget getTodayPage() {
+    return Container();
+  }
+
+  Widget getHistoryPage() {
+    return Container();
+  }
+
+  Widget getChartPage() {
+    return Container();
   }
 }
